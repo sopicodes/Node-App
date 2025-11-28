ec16b9816de43a337b0c413ac175f340886d4ba4
sophia <22-60110@g.batstate-u.edu.ph>
2025-11-28 20:16:28 +0000

    title changed

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3000;
 app.get("/", (req, res) => {
   res.send(`
     <body style="background: url('https://images.rawpixel.com/.../retrozone-bg-03-x.jpg') no-repeat center center fixed; background-size: cover;">
-    <h1>My Node.js App</h1>
+    <h1>Welcome to my Node.js Page!</h1>
     <p><strong>Name:</strong> Sophia Lorin S. Huelgas</p>
     <p><strong>Section:</strong> BSIT-BA-4101</p>
     <blockquote>"Anyone who has never made a mistake has never tried anything new."</blockquote>
